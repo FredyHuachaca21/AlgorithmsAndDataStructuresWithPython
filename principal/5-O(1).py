b462716c21b57a5ba099024bd7aa62d0c7d15fdb
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-26 11:25:49 -0500

    O(1) - Constant Time

---
+++ principal/5-O(1).py
@@ -0,0 +1,12 @@
+
+# O(1) - Constant Time - Esta función tiene una complejidad de O(1) ya que el número de operaciones que realiza es
+# "CONSTANTE" y no depende del número de elementos que recibe como parámetro.
+
+# En el siguiente ejemplo, la función add_items tiene una complejidad de O(1) ya que el número de operaciones que
+# realiza es constante, independientemente del valor de n.
+
+def add_items(n):
+    return n + n + n
+
+
+print(add_items(1))
