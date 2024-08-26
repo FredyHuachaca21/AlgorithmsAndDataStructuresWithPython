0e48655413f90ef4242cafd0336ffc3a3ccbd516
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-26 15:53:55 -0500

    Pointers  en dicionarios- Apunta a lla misma dirección en memoria

---
+++ classes/Pointer2.py
@@ -0,0 +1,14 @@
+dictionary_one = {
+    'value': 11
+}
+
+dictionary_two = dictionary_one
+
+print("Antes de actualizar el valor")
+print("dictionary_one = ", dictionary_one)
+print("dictionary_two = ", dictionary_two)
+
+print("\n dictionary_one apunta a la dirección de memoria: ", id(dictionary_one))
+print(" dictionary_two apunta a la dirección de memoria: ", id(dictionary_two))
+
+
