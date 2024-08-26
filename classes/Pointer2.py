26ae1eda01a264ac239cffa0762d0a4cb9b34996
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-26 16:00:15 -0500

    Pointers  en dicionarios- AAhora también unta a llamisma dirección en memoria

--- classes/Pointer2.py
+++ classes/Pointer2.py
@@ -11,4 +11,11 @@ print("dictionary_two = ", dictionary_two)
 print("\n dictionary_one apunta a la dirección de memoria: ", id(dictionary_one))
 print(" dictionary_two apunta a la dirección de memoria: ", id(dictionary_two))
 
+dictionary_two['value'] = 22
 
+print("\nDespués de actualizar el valor")
+print("dictionary_one = ", dictionary_one)
+print("dictionary_two = ", dictionary_two)
+
+print("\n dictionary_one apunta a la dirección de memoria: ", id(dictionary_one))
+print(" dictionary_two apunta a la dirección de memoria: ", id(dictionary_two))
