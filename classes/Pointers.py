ec9812ed251439d0f09bb959e3f65a46d4f995c1
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-26 15:39:35 -0500

    Pointers - Apunta a la misma dirección en memoria

---
+++ classes/Pointers.py
@@ -0,0 +1,9 @@
+num_one = 11
+num_two = num_one
+
+print("Antes de actualizar el valor de num_two")
+print("num_one = ", num_one)
+print("num_two = ", num_two)
+
+print("\n num_one apunta a la dirección de memoria: ", id(num_one))
+print(" num_two apunta a la dirección de memoria: ", id(num_two))
