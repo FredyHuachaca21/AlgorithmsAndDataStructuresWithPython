c1e183e045a0c8ad29f103eb5756aaa90712d7af
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-26 15:43:50 -0500

    Pointers - Apunta a ldiferente dirección en memoria

--- classes/Pointers.py
+++ classes/Pointers.py
@@ -7,3 +7,12 @@ print("num_two = ", num_two)
 
 print("\n num_one apunta a la dirección de memoria: ", id(num_one))
 print(" num_two apunta a la dirección de memoria: ", id(num_two))
+
+num_two = 22
+
+print("\nDespués de actualizar el valor de num_two")
+print("num_one = ", num_one)
+print("num_two = ", num_two)
+
+print("\n num_one apunta a la dirección de memoria: ", id(num_one))
+print(" num_two apunta a la dirección de memoria: ", id(num_two))
