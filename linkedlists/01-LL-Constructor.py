f7240dc3fef815ab4c54b6ad9dd9a6cd8a710c5b
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-26 17:50:51 -0500

    Función para imprimir la lista enlazada

--- linkedlists/01-LL-Constructor.py
+++ linkedlists/01-LL-Constructor.py
@@ -16,3 +16,11 @@ my_linked_list = LinkedList(4)
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
+
+
+def print_list(self):
+    temp = self.head
+    while temp is not None:
+        print(temp.value)
+        temp = temp.next
+
