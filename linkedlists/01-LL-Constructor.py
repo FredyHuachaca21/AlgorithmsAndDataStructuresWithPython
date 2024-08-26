d909bbb31d9cf0cdb947c847ea92d559718ee5bc
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-26 17:59:44 -0500

    Lista Enlazada - Funcioón append

--- linkedlists/01-LL-Constructor.py
+++ linkedlists/01-LL-Constructor.py
@@ -11,16 +11,28 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
-my_linked_list = LinkedList(4)
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.print_list()
+print('\n================\n')
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
-
-
-def print_list(self):
-    temp = self.head
-    while temp is not None:
-        print(temp.value)
-        temp = temp.next
-
