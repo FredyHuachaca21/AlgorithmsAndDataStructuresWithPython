146d76baac2303bcd8b93f2cd8f71f2e2ab99848
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-27 11:27:00 -0500

    LL - Prepend - Agregar un Nodo al principio

---
+++ linkedlists/04-LL-Pop.py
@@ -0,0 +1,68 @@
+class Node:
+    # Constructor
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    # Constructor
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    # Función para imprimir la lista
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    # Función para agregar un nodo
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    # Función para agregar un nodo
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+
+# (2) Items - Return 2 Nodes
+print(my_linked_list.pop().value)
+# (1) Item - Return 1 Node
+print(my_linked_list.pop().value)
+# None - Return None
+print(my_linked_list.pop())
+
+print('\n================\n')
+print('Head:', my_linked_list.head)
+print('Tail:', my_linked_list.tail)
+print('Length:', my_linked_list.length)
+
+
---
+++ linkedlists/05-LL-Prepend.py
@@ -0,0 +1,60 @@
+class Node:
+    # Constructor
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    # Constructor
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    # Función para imprimir la lista
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    # Función para agregar un nodo
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    # Función para agregar un nodo al principio de la lista
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+
+my_linked_list = LinkedList(2)
+my_linked_list.append(3)
+
+my_linked_list.prepend(1)
+
+my_linked_list.print_list()
+
+print('\n================\n')
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+
+
