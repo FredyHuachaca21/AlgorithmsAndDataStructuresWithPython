d24b4916082eeb0c4aa6e165bd7a3121213b4539
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-27 10:18:13 -0500

    Listas Enlazadas - Función prepend

--- linkedlists/01-LL-Constructor.py
+++ linkedlists/01-LL-Constructor.py
@@ -44,14 +44,23 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
 
-# (2) Items - Return 2 Nodes
-print(my_linked_list.pop().value)
-# (1) Item - Return 1 Node
-print(my_linked_list.pop().value)
-# None - Return None
-print(my_linked_list.pop())
+my_linked_list = LinkedList(2)
+my_linked_list.append(3)
+
+my_linked_list.prepend(1)
+
+my_linked_list.print_list()
+
 
