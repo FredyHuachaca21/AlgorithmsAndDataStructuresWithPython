b7cf83a0dd4e40e1ceea2421ba32c1be7d2ca16f
FREDGAR <fredy.huachaca21@gmail.com>
2024-08-27 11:16:29 -0500

    RefactorizaciÃón de la clase construcctor

--- linkedlists/01-LL-Constructor.py
+++ linkedlists/01-LL-Constructor.py
@@ -13,75 +13,10 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    # Función para imprimir la lista
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
 
-    # Función para agregar un nodo al final de la lista
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length += 1
-        return True
-
-    # Función para eliminar el último nodo de la lista
-    def pop(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        pre = self.head
-        while temp.next is not None:
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-        return temp
-
-    # Función para agregar un nodo al principio de la lista
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
-        self.length += 1
-        return True
-
-    # Función para eliminar el primer nodo de la lista
-    def pop_first(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        self.head = self.head.next
-        temp.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.tail = None
-        return temp
-
-
-my_linked_list = LinkedList(2)
-my_linked_list.append(1)
-
-# (2) Items -> Return 2 Node
-print(my_linked_list.pop_first().value)
-# (1) Item -> Return 1 Node
-print(my_linked_list.pop_first().value)
-# None -> Return None
-print(my_linked_list.pop_first())
+my_linked_list = LinkedList(4)
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
 
 
